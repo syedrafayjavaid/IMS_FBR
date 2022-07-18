6c5077d1cbb00311b9e6cf854b40ca10fa2b90f2
rsanz2022 <raul.sanz@vieuxinc.com>
2022-07-18 19:10:17 +0500

    products

--- src/app/views/Products/ProductsList.jsx
+++ src/app/views/Products/ProductsList.jsx
@@ -442,55 +442,7 @@ const ProductsList = () => {
     ]
 
     ///////add fe3atue demay data
-    const top100Films = [
-        { title: 'The Shawshank Redemption', year: 1994 },
-        { title: 'The Godfather', year: 1972 },
-        { title: 'The Godfather: Part II', year: 1974 },
-        { title: 'The Dark Knight', year: 2008 },
-        { title: '12 Angry Men', year: 1957 },
-        { title: "Schindler's List", year: 1993 },
-        { title: 'Pulp Fiction', year: 1994 },
-
-        { title: 'City of God', year: 2002 },
-        { title: 'Se7en', year: 1995 },
-        { title: 'The Silence of the Lambs', year: 1991 },
-        { title: "It's a Wonderful Life", year: 1946 },
-        { title: 'Life Is Beautiful', year: 1997 },
-        { title: 'The Usual Suspects', year: 1995 },
-        { title: 'Léon: The Professional', year: 1994 },
-        { title: 'Spirited Away', year: 2001 },
-        { title: 'Saving Private Ryan', year: 1998 },
-        { title: 'Once Upon a Time in the West', year: 1968 },
-        { title: 'American History X', year: 1998 },
-        { title: 'Interstellar', year: 2014 },
-        { title: 'Casablanca', year: 1942 },
-        { title: 'City Lights', year: 1931 },
-        { title: 'Psycho', year: 1960 },
-        { title: 'The Green Mile', year: 1999 },
-        { title: 'The Intouchables', year: 2011 },
-        { title: 'Modern Times', year: 1936 },
-        { title: 'Raiders of the Lost Ark', year: 1981 },
-        { title: 'Rear Window', year: 1954 },
-        { title: 'The Pianist', year: 2002 },
-        { title: 'The Departed', year: 2006 },
-        { title: 'Terminator 2: Judgment Day', year: 1991 },
-        { title: 'Back to the Future', year: 1985 },
-        { title: 'Whiplash', year: 2014 },
-        { title: 'Gladiator', year: 2000 },
-        { title: 'Memento', year: 2000 },
-        { title: 'The Prestige', year: 2006 },
-        { title: 'The Lion King', year: 1994 },
-        { title: 'Apocalypse Now', year: 1979 },
-        { title: 'Alien', year: 1979 },
-        { title: 'Sunset Boulevard', year: 1950 },
-
-        { title: 'Once Upon a Time in America', year: 1984 },
-        { title: 'Witness for the Prosecution', year: 1957 },
-        { title: 'Das Boot', year: 1981 },
-        { title: 'Citizen Kane', year: 1941 },
-        { title: 'North by Northwest', year: 1959 },
-        { title: 'Vertigo', year: 1958 },
-    ]
+   
     return (
         <>
             {open && (
@@ -780,28 +732,7 @@ const ProductsList = () => {
                           
                         </Grid>
                         </Grid>
-                        <br></br>
-                        <Grid container spacing={3}>
-                           
-                        <Grid item lg={12} md={12} sm={12} xs={12}>
-                                <Autocomplete
-                                    multiple
-                                    limitTags={2}
-                                    id="multiple-limit-tags"
-                                    options={top100Films}
-                                    getOptionLabel={(option) => option.title}
-                                    renderInput={(params) => (
-                                        <TextField
-                                       // value={features}
-                                            {...params}
-                                            label="features suggestive"
-                                            placeholder="features suggestive"
-                                            // onClick={guardarNumeros()}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                            </Grid>
+                       
                             <br></br>
                         <Grid container spacing={3}>
                         <Grid item lg={12} md={12} sm={12} xs={12}>
